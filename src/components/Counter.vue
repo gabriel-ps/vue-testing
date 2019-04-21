6759f9ffa04e9cfb4e4266ea68f8486b9d9a9d11
Gabriel Pereira da Silva <gabriel.gps@outlook.com>
2019-04-21 19:16:29 -0300

    Test Single File Components.

---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <span class="count">{{ count }}</span>
+        <button @click="count++">Increment</button>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        };
+    }
+}
+</script>
